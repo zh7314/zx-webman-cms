68d8bea8a20e5eaf2da52080022733e834d498cd
ZX <903464207@qq.com>
2023-09-08 16:20:50 +0800

    1

--- webman/app/middleware/CrossDomain.php
+++ webman/app/middleware/CrossDomain.php
@@ -13,25 +13,25 @@ class  CrossDomain implements MiddlewareInterface
     public function process(Request $request, callable $next): Response
     {
 
-//        $header = [
-//            'Access-Control-Allow-Credentials' => 'true',
-//            'Access-Control-Allow-Origin' => $request->header('origin', '*'),
-//            'Access-Control-Allow-Methods' => $request->header('access-control-request-method', '*'),
-//            'Access-Control-Allow-Headers' => $request->header('access-control-request-headers', '*'),
-//        ];
-//
-//        if ($request->method() === 'OPTIONS') {
-//            return response('200', 200, $header);
-//        } else {
-//            $response = $next($request);
-//            $response->withHeaders($header);
-//            return $response;
-//        }
-        header('Access-Control-Allow-Credentials: true');
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
-        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With');
+        $header = [
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Origin' => $request->header('origin', '*'),
+            'Access-Control-Allow-Methods' => $request->header('access-control-request-method', '*'),
+            'Access-Control-Allow-Headers' => $request->header('access-control-request-headers', '*'),
+        ];
 
-        return $next($request);
+        if ($request->method() === 'OPTIONS') {
+            return response('', 200, $header);
+        } else {
+            $response = $next($request);
+            $response->withHeaders($header);
+            return $response;
+        }
+
+//        header('Access-Control-Allow-Credentials: true');
+//        header('Access-Control-Allow-Origin: *');
+//        header('Access-Control-Allow-Methods: GET,POST,PUT,DELETE,OPTIONS');
+//        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Requested-With');
+//        return $next($request);
     }
 }
